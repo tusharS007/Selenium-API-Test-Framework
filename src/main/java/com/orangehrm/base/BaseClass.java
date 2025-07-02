2cab49dd395cdc176fd7230aafae8ff15354b3bb
tushar <tdshelar007@gmail.com>
2025-07-02 19:11:21 +0530

    edoptions are updated for headless 3rd run

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -143,7 +143,7 @@ public class BaseClass {
 			} else if (browser.equalsIgnoreCase("edge")) {
 
 				EdgeOptions options = new EdgeOptions();
-				options.addArguments("--headless=new"); // Run Edge in headless mode
+				options.addArguments("--headless"); // Run Edge in headless mode
 				isHeadless = true;
 				options.addArguments("--disable-gpu"); // Disable GPU acceleration
 				options.addArguments("--window-size=1920,1080"); // Set window size
