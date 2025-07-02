60245985c01e448ea5ad069b6922e5c724f80cee
tushar <tdshelar007@gmail.com>
2025-07-02 12:18:11 +0530

    test checkin 2

--- src/test/java/com/orangehrm/test/DummyClass2.java
+++ src/test/java/com/orangehrm/test/DummyClass2.java
@@ -10,6 +10,7 @@ public class DummyClass2 extends BaseClass {
 	@Test
 	public void dummyTEst() {
 		// ExtentManager.startTest("Dummy Test2"); - This has been implemented in test Listener
+		//using ngrok url
 		String title = getDriver().getTitle();
 		ExtentManager.logStep("Verifying title");
 		Assert.assertEquals(title, "OrangeHRM");
